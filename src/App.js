305850fb29fd1aee83320cd6c0717687a7c124e2
Jonathan Gabriel Budiman <jojgb96@gmail.com>
2020-10-29 20:29:47 +0700

    first modul done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HelloWorld(props){
+  return <div>{props.kata}</div>
+}
+function Button(){
+  return <button onClick={()=>alert('Youkoso')}>Jajal Ketok Inyong</button>
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <HelloWorld kata="Saha Maneh"/>
+        <Button />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Hello Jonathan
         </p>
         <a
           className="App-link"
